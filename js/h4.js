468d483867334acb3c92705f34c4f241de794222
Rozhynskyy Nikita <mykyta.rozhynskyy@loyd.international>
2016-06-23 16:58:14 +0300

    drag and drop, should show video

--- js/h4.js
+++ js/h4.js
@@ -1,28 +1,48 @@
-//var player;
-//$(document).ready(function () {
-////    selectChange();
-//})
-//
-////function getCamUri() {
-////    $.get('/h4l/get_group_ids', {}, function (data) {
-////        console.log(data);
-////        uri = data.response;
-////        var client = new WebSocket(uri);
-////        var canvas = document.getElementById('vc1');
-////        player = new jsmpeg(client, {
-////            canvas: canvas
-////        });
-////    });
-////}
-//
-////function selectChange(){
-////     $('.tab select').change(function () {
-////        var val = $(this).val();
-////        var url = $(this).parent().attr('uri');
-////        var canvas = document.getElementById('vc'+val);
-////        var client = new WebSocket(url);
-////        player = new jsmpeg(client, {
-////            canvas: canvas
-////        });
-////    })
-////}
+    /* WORK WITH CAMS */
+    
+    /* build li tag groups and cams*/
+    function liTag(){
+        var arr;
+        arr = '{"state":"ok","response":[{"id":1,"name":"Ферма","cams":[{"id":1, "name":"поросята"},{"id":2, "name":"телята"}]},{"id":2,"name":"Охорона","cams":[{"id":3, "name":"ворота"},{"id":4, "name":"пес"}]}]}';
+        arr = JSON.parse(arr);
+        arr = arr.response;
+            $('#group_cams li').remove();
+            for( var i = 0; i < arr.length; i++){
+                $('#group_cams').append('<h4>' + arr[i].name + '</h4>');
+                for (var q = 0; q < arr[i].cams.length; q++){
+                    $('#group_cams').append('<li url="' + arr[i].cams[q].id + '"><img src="./img/cam_green.png" alt="">' + arr[i].cams[q].name + '</li>');
+                }
+            }
+    }
+
+    /* drag and drop cams to canvas tag */
+    function drug(){
+        $( "#group_cams li" ).draggable({
+            revert:true
+        });
+
+        $( ".canvas" ).droppable({
+          drop: function( event, ui ) {
+              var id = ui.draggable.attr('url');
+              var text = ui.draggable.text();
+              var id_canvas = $(this).attr('id');
+              var data = {
+                  id: id
+              }
+              $.get('/get_cam_uri', data, function(data){
+                  if(data.state == 'ok'){
+                    var uri = data.response;
+                    var client = new WebSocket(uri);
+                    var canvas = document.getElementById(id_canvas);
+                    var player = new jsmpeg(client, {
+                        canvas: canvas
+                    });
+                  }else{
+                      throw ("I can't to download by url")
+                  }
+              })
+          }
+        });
+    }
+    /* build select */
+    
--- js/sripts.js
+++ js/sripts.js
@@ -147,80 +147,36 @@ $(document).ready(function () {
         $(this).next().toggle('slow');
     })
     
-    /* grid sistem */
-    /* WORK WITH CAMS */
-    var grid_1 = '<div class="row_1"><div class="col-xs-12"><canvas id="vc1"></canvas></div></div>';
-    var grid_2 = '<div class="row_2"><div class="col-xs-6"><canvas id="vc1"></canvas></div><div class="col-xs-6"><canvas id="vc2"></canvas></div></div><div class="row_2"><div class="col-xs-6"><canvas id="vc3"></canvas></div><div class="col-xs-6"><canvas id="vc4"></canvas></div></div>';
-    var grid_3 = '<div class="row_3"><div class="col-xs-6"><canvas id="vc1"></canvas></div><div class="col-xs-6"><canvas id="vc2"></canvas></div></div><div class="row_3"><div class="col-xs-6"><canvas id="vc3"></canvas></div><div class="col-xs-6"><canvas id="vc4"></canvas></div></div><div class="row_3"><div class="col-xs-6"><canvas id="vc5"></canvas></div><div class="col-xs-6"><canvas id="vc6"></canvas></div></div>';
+    /* grid sistem 1x1 2x2 3x3 */
+    var grid_1 = '<div class="row_1"><div class="col-xs-12"><canvas class="canvas" id="vc1"></canvas></div></div>';
+    var grid_2 = '<div class="row_2"><div class="col-xs-6"><canvas class="canvas" id="vc1"></canvas></div><div class="col-xs-6"><canvas class="canvas" id="vc2"></canvas></div></div><div class="row_2"><div class="col-xs-6"><canvas class="canvas" id="vc3"></canvas></div><div class="col-xs-6"><canvas class="canvas" id="vc4"></canvas></div></div>';
+    var grid_3 = '<div class="row_3"><div class="col-xs-6"><canvas class="canvas" id="vc1"></canvas></div><div class="col-xs-6"><canvas class="canvas" id="vc2"></canvas></div></div><div class="row_3"><div class="col-xs-6"><canvas class="canvas" id="vc3"></canvas></div><div class="col-xs-6"><canvas class="canvas" id="vc4"></canvas></div></div><div class="row_3"><div class="col-xs-6"><canvas class="canvas" id="vc5"></canvas></div><div class="col-xs-6"><canvas class="canvas" id="vc6"></canvas></div></div>';
     
-    /* event on change select */
-    function selectChange(){
-         $('.tab select').change(function () {
-            var player;
-            var val = $(this).val();
-            var url = $(this).parent().attr('uri');
-            var canvas = document.getElementById('vc'+val);
-            var client = new WebSocket(url);
-            player = new jsmpeg(client, {
-                canvas: canvas
-            });
-             console.log(url)
-        })
-    }
-    
-    /* build li tag */
-    function liTag(){
-        var uri;
-        $.get('/h4l/get_group_ids', {}, function (data) {
-            uri = data.response;
-            $('.tab li').remove();
-            for( var i = 0; i < uri.length; i++){
-                $('.tab').append('<li uri="' + uri[i] + '"><img src="./img/cam_green.png" alt="">Хлев<select name="" ><option value=""></option><option value="1">1</option></select></li>')
-            }
-            selectChange();
-        });
-        
-    }
-    liTag()
-    /* build select */
     $('.listDisplay li').click(function(){
         var val = $(this).attr('val');
         var container = $('.grid');
-        var selects;
-        var count = 0;
         
-        function selectsGrid(){
-            $('.tab li select').html('<option  value=""></option>');
-            for(var i = 0; i < parseFloat(val); i++){
-                count++
-                $('.tab li select').append('<option value="' + count + '">' + count + '</option>')
-            }
-        }
         switch (val){
             case '1':
                 container.html(grid_1);
-                selectsGrid();
-                selectChange();
+                drug()
                 break;
             case '4':
                 console.log('work');
                 container.html(grid_2);
-                selectsGrid();
-                selectChange();
+                drug()
                 break;
             case '6':
                 container.html(grid_3);
-                selectsGrid();
-                selectChange();
+                drug()
                 break;
         }
     })
+    /* WORK WITH CAMS */
+    liTag();
+    drug();
+
 })
-
-
-
-
-
 
 
 
