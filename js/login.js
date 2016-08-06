593c627d9d4f5ab7edcfa24a6485c6478a5b17eb
Rozhynskyy Nikita <mykyta.rozhynskyy@loyd.international>
2016-08-06 13:01:15 +0300

    change settings for login.js file

--- js/login.js
+++ js/login.js
@@ -1,24 +1,22 @@
 var domain = 'http://h4l.paliy.lviv.ua';
 //var domain = '';
-
 $(document).ready(function () {
-    $(document).keypress(function(e) {
-    if(e.which == 13) {
-    /* Authorization */
-    $('#send_aut').click(function () {
-        var password = $('#password').val();
-        var login = $('#login').val();
-        var data = {
-            username: login,
-            password: password
-        }
-        if (password && login) {
-            $.post(domain + '/h4l/login', data, function (data) {
-                if(data.state == 'ok'){
-                    window.location.assign('/cameras.html')
-                }
-            })
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            /* Authorization */
+            var password = $('#password').val();
+            var login = $('#login').val();
+            var data = {
+                username: login
+                , password: password
+            }
+            if (password && login) {
+                $.post(domain + '/h4l/login', data, function (data) {
+                    if (data.state == 'ok') {
+                        window.location.assign('/cameras.html')
+                    }
+                })
+            }
         }
     })
-    }
 })
